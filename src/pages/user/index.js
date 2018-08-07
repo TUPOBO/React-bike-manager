1b5ccbd8f4cff622517003a997f3e4ea684404ea
TUPOBO <1028461931@qq.com>
2018-08-07 23:40:58 +0800

    实现员工编辑 详情 删除

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Card, Button, Modal, Form, Input, Radio, DatePicker, Select } from 'antd'
+import moment from 'moment'
 import axios from './../../axios'
 import Utils from './../../utils/utils'
 import BaseForm from './../../components/BaseForm'
@@ -55,33 +56,96 @@ class User extends Component {
 	}
 
 	handleOperate = (type) => {
+    let _this = this
+		let item = this.state.selectedItem
 		if (type === 'create') {
 			this.setState({
 				type,
 				isVisible: true,
 				title: '创建员工'
 			})
+		} else if (type === 'edit') {
+			if (!item) {
+				Modal.info({
+					title: '提示',
+					content: '请选择一个用户'
+				})
+				return
+			}
+			this.setState({
+				type,
+				isVisible: true,
+				title: '创建员工',
+				userInfo: item
+			})
+		} else if (type === 'detail') {
+			if (!item) {
+				Modal.info({
+					title: '提示',
+					content: '请选择一个用户'
+				})
+				return
+			}
+			this.setState({
+				type,
+				isVisible: true,
+				title: '员工详情',
+				userInfo: item
+			})
+		} else {
+			if (!item) {
+				Modal.info({
+					title: '提示',
+					content: '请选择一个用户'
+				})
+				return
+			}
+			Modal.confirm({
+        title: '确认删除',
+        content: '是否删除所选中的员工',
+				onOk() {
+					axios
+						.ajax({
+							url: '/user/delete',
+							data: {
+								params: {
+									id: item.id
+								}
+							}
+						})
+						.then((res) => {
+							if (res.code === 0) {
+								_this.setState({
+									isVisible: false
+                })
+                _this.requestList()
+							}
+						})
+				}
+			})
 		}
-  }
+	}
 
-  //创建员工提交
-  handleSubmit = () => {
-    let type = this.state.type
-    let data = this.userForm.props.form.getFieldsValue()
-    axios.ajax({
-      url: '/user/add',
-      data: {
-        params: data
-      }
-    }).then((res) => {
-      if (res.code === 0) {
-        this.setState({
-          isVisible: false
-        })
-        this.requestList()
-      }
-    })
-  }
+	//创建员工提交
+	handleSubmit = () => {
+		let type = this.state.type
+		let data = this.userForm.props.form.getFieldsValue()
+		axios
+			.ajax({
+				url: type === 'create' ? '/user/add' : '/user/edit',
+				data: {
+					params: data
+				}
+			})
+			.then((res) => {
+				if (res.code === 0) {
+					this.setState({
+						isVisible: false
+					})
+					this.requestList()
+				}
+			})
+	}
 
 	render() {
 		const columns = [
@@ -144,6 +208,12 @@ class User extends Component {
 				dataIndex: 'time'
 			}
 		]
+		let footer = {}
+		if (this.state.type === 'detail') {
+			footer = {
+				footer: null
+			}
+		}
 		return (
 			<div>
 				<Card>
@@ -178,15 +248,17 @@ class User extends Component {
 					visible={this.state.isVisible}
 					onOk={this.handleSubmit}
 					onCancel={() => {
-            this.userForm.props.resetFields()
+						this.userForm.props.form.resetFields()
 						this.setState({
 							isVisible: false
 						})
 					}}
 					width={600}
+					{...footer}
 				>
 					<UserForm
 						type={this.state.type}
+						userInfo={this.state.userInfo}
 						wrappedComponentRef={(inst) => {
 							this.userForm = inst
 						}}
@@ -199,6 +271,8 @@ class User extends Component {
 
 class UserForm extends Component {
 	render() {
+		let type = this.props.type
+		let userInfo = this.props.userInfo || {}
 		const formItemLayout = {
 			labelCol: { span: 5 },
 			wrapperCol: { span: 15 }
@@ -208,36 +282,70 @@ class UserForm extends Component {
 			<div>
 				<Form layout="horizontal">
 					<FormItem label="用户名" {...formItemLayout}>
-						{getFieldDecorator('user_name')(<Input type="text" placeholder="请输入用户名" />)}
+						{type === 'detail' ? (
+							userInfo.userName
+						) : (
+							getFieldDecorator('user_name', {
+								initialValue: userInfo.userName
+							})(<Input type="text" placeholder="请输入用户名" />)
+						)}
 					</FormItem>
 					<FormItem label="性别" {...formItemLayout}>
-						{getFieldDecorator('sex', {
-							initialValue: 1
-						})(
-							<RadioGroup>
-								<Radio value={1}>男</Radio>
-								<Radio value={2}>女</Radio>
-							</RadioGroup>
+						{type === 'detail' ? userInfo.sex === 1 ? (
+							'男'
+						) : (
+							'女'
+						) : (
+							getFieldDecorator('sex', {
+								initialValue: userInfo.sex
+							})(
+								<RadioGroup>
+									<Radio value={1}>男</Radio>
+									<Radio value={2}>女</Radio>
+								</RadioGroup>
+							)
 						)}
 					</FormItem>
 					<FormItem label="状态" {...formItemLayout}>
-						{getFieldDecorator('state', {
-							initialValue: 1
-						})(
-							<Select>
-								<Option value={1}>咸鱼一条</Option>
-								<Option value={2}>风华浪子</Option>
-								<Option value={3}>北大才子</Option>
-								<Option value={4}>百度FE</Option>
-								<Option value={5}>创业者</Option>
-							</Select>
+						{type === 'detail' ? (
+							{
+								'1': '咸鱼一条',
+								'2': '风华浪子',
+								'3': '北大才子',
+								'4': '百度FE',
+								'5': '创业者'
+							}[userInfo.state]
+						) : (
+							getFieldDecorator('state', {
+								initialValue: userInfo.state
+							})(
+								<Select>
+									<Option value={1}>咸鱼一条</Option>
+									<Option value={2}>风华浪子</Option>
+									<Option value={3}>北大才子</Option>
+									<Option value={4}>百度FE</Option>
+									<Option value={5}>创业者</Option>
+								</Select>
+							)
 						)}
 					</FormItem>
 					<FormItem label="生日" {...formItemLayout}>
-						{getFieldDecorator('birthday')(<DatePicker />)}
+						{type === 'detail' ? (
+							userInfo.birthday
+						) : (
+							getFieldDecorator('birthday', {
+								initialValue: moment(userInfo.birthday)
+							})(<DatePicker />)
+						)}
 					</FormItem>
 					<FormItem label="地址" {...formItemLayout}>
-						{getFieldDecorator('address')(<TextArea rows={3} placeholder="请输入联系地址" />)}
+						{type === 'detail' ? (
+							userInfo.address
+						) : (
+							getFieldDecorator('address', {
+								initialValue: userInfo.address
+							})(<TextArea rows={3} placeholder="请输入联系地址" />)
+						)}
 					</FormItem>
 				</Form>
 			</div>
