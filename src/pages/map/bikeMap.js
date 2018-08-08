71f585168d2b3ee5ae851724caaab3177f8a2d3f
TUPOBO <1028461931@qq.com>
2018-08-08 21:51:33 +0800

    实现地图数据渲染

--- src/pages/map/bikeMap.js
+++ src/pages/map/bikeMap.js
@@ -6,8 +6,8 @@ import axios from './../../axios'
 const BMap = window.BMap
 class BikeMap extends Component {
 	constructor(props) {
-    super(props)
-    this.state = {}
+		super(props)
+		this.state = {}
 		this.formList = [
 			{
 				type: 'SELECT',
@@ -36,45 +36,109 @@ class BikeMap extends Component {
 				list: [ { id: '0', name: '全部' }, { id: '1', name: '进行中' }, { id: '2', name: '结束行程' } ]
 			}
 		]
-  }
+	}
 
-
-  componentDidMount () {
-    this.requestList()
-  }
-
+	componentDidMount() {
+		this.requestList()
+	}
 
 	handleFilterSubmit = (params) => {
-    this.params = params
-    this.requestList()
-  }
+		this.params = params
+		this.requestList()
+	}
 
-  requestList = () => {
-    axios.ajax({
-      url: '/map/bike_list',
-      data: {
-        params: this.params
-      }
-    }).then ((res) => {
-      if (res.code === 0) {
-        this.setState({
-          totalCount: res.result.total_count
-        })
-        this.renderMap(res)
-      }
-    })
-  }
+	requestList = () => {
+		axios
+			.ajax({
+				url: '/map/bike_list',
+				data: {
+					params: this.params
+				}
+			})
+			.then((res) => {
+				if (res.code === 0) {
+					this.setState({
+						totalCount: res.result.total_count
+					})
+					this.renderMap(res)
+				}
+			})
+	}
 
-  renderMap = (res) => {
-    let list = res.result.route_list
-    this.map = new BMap.Map('container')
-    let startGps = list[0].split(',')
-    let endGps = list[list.length-1].split(',')
-    let startPoint = new BMap.Point(startGps[0], startGps[1])
-    let endPoint = new BMap.Point(endGps[0], endGps[1])
+	renderMap = (res) => {
+		// 百度地图初始化
+		let list = res.result.route_list
+		this.map = new BMap.Map('container')
+		let startGps = list[0].split(',')
+		let endGps = list[list.length - 1].split(',')
+		let startPoint = new BMap.Point(startGps[0], startGps[1])
+		let endPoint = new BMap.Point(endGps[0], endGps[1])
+		this.map.centerAndZoom(endPoint, 11)
 
-    this.map.centerAndZoom(endPoint, 11)
-  }
+		// 绘制起点坐标
+		// let startPointIcon = new BMap.Icon(
+		// 	'/assets/start_point.png',
+		// 	new BMap.Size((36, 42), {
+		// 		imageSize: new BMap.Size(36, 42),
+		// 		anchor: new BMap.Size(18, 42)
+		// 	})
+		// )
+		// let bikeStartMarker = new BMap.Marker(startPoint, { icon: startPointIcon })
+		// this.map.addOverlay(bikeStartMarker)
+
+		// // 绘制终点坐标
+		// let endPointIcon = new BMap.Icon(
+		// 	'/assets/end_point.png',
+		// 	new BMap.Size((36, 42), {
+		// 		anchor: new BMap.Size(10, 42),
+		// 		imageSize: new BMap.Size(36, 42)
+		// 	})
+		// )
+		// let bikeEndMarker = new BMap.Marker(startPoint, { icon: endPointIcon })
+		// this.map.addOverlay(bikeEndMarker)
+
+		//绘制车辆行驶路线
+		let routeList = []
+		list.forEach((item) => {
+			let point = item.split(',')
+			routeList.push(new BMap.Point(point[0], point[1]))
+		})
+		let polyLine = new BMap.Polyline(routeList, {
+			strokeColor: '#ef4136',
+			strokeWeight: 3,
+			strokeOpacity: 1
+		})
+		this.map.addOverlay(polyLine)
+
+		// 绘制服务区
+		let servicePoint = []
+		let serviceList = res.result.service_list
+		serviceList.forEach((item) => {
+			servicePoint.push(new BMap.Point(item.lon, item.lat))
+		})
+		let servicePolyLine = new BMap.Polyline(servicePoint, {
+			strokeColor: '#ef4136',
+			strokeWeight: 3,
+			strokeOpacity: 1
+		})
+		this.map.addOverlay(servicePolyLine)
+
+		// 绘制自行车图标
+		let bikeList = res.result.bike_list
+		let bikeIcon = new BMap.Icon(
+			'/assets/bike.jpg',
+			  new BMap.Size((36, 42), {
+				anchor: new BMap.Size(36, 42),
+				imageSize: new BMap.Size(36, 42)
+			})
+		)
+		bikeList.forEach((item) => {
+			let point = item.split(',')
+			let markerPoint = new BMap.Point(point[0], point[1])
+			let bikeMarker = new BMap.Marker(markerPoint)
+			this.map.addOverlay(bikeMarker)
+		})
+	}
 
 	render() {
 		return (
@@ -82,9 +146,9 @@ class BikeMap extends Component {
 				<Card>
 					<BaseForm formList={this.formList} filterSubmit={this.handleFilterSubmit} />
 				</Card>
-				<Card style={{marginTop: 10}}>
-					<div>共{this.state.total_count}辆单车</div>
-					<div id='container' style={{ height: 500 }} />
+				<Card style={{ marginTop: 10 }}>
+					<div>共{this.state.totalCount}辆单车</div>
+					<div id="container" style={{ height: 600 }} />
 				</Card>
 			</div>
 		)
